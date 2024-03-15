a67523696efb1955c05c4907eec7d0389cdc89ac
Martin Lado <ladomartin416@gmail.com>
2024-03-15 13:17:52 -0700

    fixed file names

--- question1/PizzaDriver1.java
+++ question1/PizzaDriver1.java
@@ -1,36 +1,35 @@
 package question1;
-
 public class PizzaDriver1 {
     public static void main(String[] args) {
-        //first pizza with 3 toppings on size small 
+        //PIZZA HUT
+        // Large + 3 toppings
         PizzaBuilder pizzaBuilder1 = new PizzaBuilder();
         pizzaBuilder1.setChainName("Pizza Hut");
-        pizzaBuilder1.setSize("Small");
-        pizzaBuilder1.addMushrooms();
-        pizzaBuilder1.addPepperoni();
-        pizzaBuilder1.addSausage();
+        pizzaBuilder1.setSize("Large");
+
+        pizzaBuilder1.addBacon();
+        pizzaBuilder1.addBeef();
+        pizzaBuilder1.addChicken();
+
         Pizza pizza1 = pizzaBuilder1.build();
         pizza1.eat();
 
-        //second pizza with 6 toppings on size medium  
+        // Small + 2 toppings
         PizzaBuilder pizzaBuilder2 = new PizzaBuilder();
         pizzaBuilder2.setChainName("Pizza Hut");
-        pizzaBuilder2.setSize("Medium");
+        pizzaBuilder2.setSize("Small");
 
-        pizzaBuilder2.addMushrooms();
-        pizzaBuilder2.addPepperoni();
-        pizzaBuilder2.addSausage();
         pizzaBuilder2.addBacon();
-        pizzaBuilder2.addOnions();
-        pizzaBuilder2.addExtraCheese();
+        pizzaBuilder2.addBeef();
 
         Pizza pizza2 = pizzaBuilder2.build();
         pizza2.eat();
 
-        //third pizza with 9 toppings on size large  
+        //LITTLE CEASARS
+        // Medium + 8 toppings
         PizzaBuilder pizzaBuilder3 = new PizzaBuilder();
-        pizzaBuilder3.setChainName("Pizza Hut");
-        pizzaBuilder3.setSize("Large");
+        pizzaBuilder3.setChainName("Little Ceasars");
+        pizzaBuilder3.setSize("Medium");
 
         pizzaBuilder3.addMushrooms();
         pizzaBuilder3.addPepperoni();
@@ -40,9 +39,47 @@ public class PizzaDriver1 {
         pizzaBuilder3.addExtraCheese();
         pizzaBuilder3.addPesto();
         pizzaBuilder3.addPeppers();
-        pizzaBuilder3.addOlives();
         
         Pizza pizza3 = pizzaBuilder3.build();
         pizza3.eat();
+        // Small + 6 toppings
+        PizzaBuilder pizzaBuilder4 = new PizzaBuilder();
+        pizzaBuilder4.setChainName("Little Ceasars");
+        pizzaBuilder4.setSize("Small");
+
+        pizzaBuilder4.addMushrooms();
+        pizzaBuilder4.addPepperoni();
+        pizzaBuilder4.addSausage();
+        pizzaBuilder4.addBacon();
+        pizzaBuilder4.addOnions();
+        pizzaBuilder4.addExtraCheese();
+
+        Pizza pizza4 = pizzaBuilder4.build();
+        pizza4.eat();
+
+        //DOMINOS
+        // Small + 1 topping
+        PizzaBuilder pizzaBuilder5 = new PizzaBuilder();
+        pizzaBuilder5.setChainName("Dominos");
+        pizzaBuilder5.setSize("Small");
+
+        pizzaBuilder5.addMushrooms();
+
+        Pizza pizza5 = pizzaBuilder5.build();
+        pizza5.eat();
+
+        // Large + 3 toppings
+        PizzaBuilder pizzaBuilder6 = new PizzaBuilder();
+        pizzaBuilder6.setChainName("Dominos");
+        pizzaBuilder6.setSize("Large");
+
+        pizzaBuilder6.addMushrooms();
+        pizzaBuilder6.addPepperoni();
+        pizzaBuilder6.addSausage();
+
+        Pizza pizza6 = pizzaBuilder6.build();
+        pizza6.eat();
     }
 }
+
+
---
+++ question1/PizzaDriver2.java
@@ -0,0 +1,48 @@
+package question1;
+
+public class PizzaDriver2 {
+    public static void main(String[] args) {
+        //first pizza with 3 toppings on size small 
+        PizzaBuilder pizzaBuilder1 = new PizzaBuilder();
+        pizzaBuilder1.setChainName("Pizza Hut");
+        pizzaBuilder1.setSize("Small");
+        pizzaBuilder1.addMushrooms();
+        pizzaBuilder1.addPepperoni();
+        pizzaBuilder1.addSausage();
+        Pizza pizza1 = pizzaBuilder1.build();
+        pizza1.eat();
+
+        //second pizza with 6 toppings on size medium  
+        PizzaBuilder pizzaBuilder2 = new PizzaBuilder();
+        pizzaBuilder2.setChainName("Pizza Hut");
+        pizzaBuilder2.setSize("Medium");
+
+        pizzaBuilder2.addMushrooms();
+        pizzaBuilder2.addPepperoni();
+        pizzaBuilder2.addSausage();
+        pizzaBuilder2.addBacon();
+        pizzaBuilder2.addOnions();
+        pizzaBuilder2.addExtraCheese();
+
+        Pizza pizza2 = pizzaBuilder2.build();
+        pizza2.eat();
+
+        //third pizza with 9 toppings on size large  
+        PizzaBuilder pizzaBuilder3 = new PizzaBuilder();
+        pizzaBuilder3.setChainName("Pizza Hut");
+        pizzaBuilder3.setSize("Large");
+
+        pizzaBuilder3.addMushrooms();
+        pizzaBuilder3.addPepperoni();
+        pizzaBuilder3.addSausage();
+        pizzaBuilder3.addBacon();
+        pizzaBuilder3.addOnions();
+        pizzaBuilder3.addExtraCheese();
+        pizzaBuilder3.addPesto();
+        pizzaBuilder3.addPeppers();
+        pizzaBuilder3.addOlives();
+        
+        Pizza pizza3 = pizzaBuilder3.build();
+        pizza3.eat();
+    }
+}
